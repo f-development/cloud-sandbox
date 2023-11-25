0ee30ccbfc8d76331a0b6a8cab28d54546892394
KEEWOONG NAM <keewoong.nam@flicspy.com>
2023-11-25 17:24:42 +0900

    Custom error

--- go/main.go
+++ go/main.go
@@ -6,9 +6,20 @@ import (
 	"github.com/pkg/errors"
 )
 
+type error1 struct {
+}
+
+func (*error1) Error() string {
+	return "error 1"
+}
+
+func Error1() error {
+	return &error1{}
+}
+
 func main() {
-	err1 := fmt.Errorf("err 1")
-	err2 := errors.Wrapf(err1, "err 2")
+	err1 := errors.WithStack(fmt.Errorf("err 1"))
+	err2 := errors.Wrapf(err1, "")
 	err3 := errors.Wrapf(err2, "err 3")
 	fmt.Printf("%+v\n", err1)
 	fmt.Printf("%+v\n", err2)
